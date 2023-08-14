4a84222da307dfe090345514616e08ce20d4c1c3
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-08-14 20:58:51 +0530

    Added anchor tag which gives a problem of reloading and sending new http request which inturn changes the app level state which is not good in nextJs

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,11 @@ export default function Home() {
   return (
     <>
       <h1>Hi I am Next App</h1>
+      <ul>
+        <li>
+            <a href="/clients">Clients</a>
+        </li>
+    </ul>
     </>
   );
 }
