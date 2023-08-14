615b13741418bb5651532caa4e9664c359ac6391
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-08-14 13:01:35 +0530

    Accessed router.pathname property

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,10 @@
+import { useRouter } from 'next/router';
+
 function ProjectFile() {
+
+    const router = useRouter();
+    console.log(router.pathname)
+
     return <><h1>
         I am project id page
     </h1>
