eff2c184450aa834bdad45e64ad4aec5560aa416
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-08-16 05:39:50 +0530

    Practiced replace attribute in Link

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <h1>Hi I am Next App</h1>
       <ul>
         <li>
-            <a href="/clients">Clients</a>
+            <Link replace href="/clients">Clients</Link>
         </li>
     </ul>
     </>
